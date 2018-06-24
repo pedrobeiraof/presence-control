37bec990dca973f8f2f760804baa0df8a710637f
Pedro Fernandes <pedrobeiraof@Pedros-MacBook-Pro.local>
2018-06-24 13:04:37 -0300

    Add style

--- presence_control/settings.py
+++ presence_control/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATICFILES_DIRS = (
+    BASE_DIR + '/static',
+)
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR + '/media/'
+MEDIA_URL = '/media/'
--- presence_control/urls.py
+++ presence_control/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 # from django.conf.urls import url, include
 
 urlpatterns = [
@@ -22,3 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'',  include('controll.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
